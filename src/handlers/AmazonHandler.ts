373340191aa6fcd8d9528f8cb8acd0fc51e6078e
Sayan Mondal <sayan.ee.nitd@gmail.com>
2022-06-08 12:20:00 +0530

    filter jobs more than a month old

--- src/handlers/AmazonHandler.ts
+++ src/handlers/AmazonHandler.ts
@@ -1,7 +1,8 @@
 import { Handler, Job } from "../types";
 import axios from 'axios';
 
-const url = 'https://www.amazon.jobs/en/search.json?radius=24km&facets%5B%5D=normalized_country_code&facets%5B%5D=normalized_state_name&facets%5B%5D=normalized_city_name&facets%5B%5D=location&facets%5B%5D=business_category&facets%5B%5D=category&facets%5B%5D=schedule_type_id&facets%5B%5D=employee_class&facets%5B%5D=normalized_location&facets%5B%5D=job_function_id&facets%5B%5D=is_manager&facets%5B%5D=is_intern&offset=0&result_limit=10&sort=relevant&latitude=&longitude=&loc_group_id=&loc_query=India&base_query=Software%20Development&city=&country=IND&region=&county=&query_options=&';
+const resLimit = 100;
+const url = `https://www.amazon.jobs/en/search.json?radius=24km&facets%5B%5D=normalized_country_code&facets%5B%5D=normalized_state_name&facets%5B%5D=normalized_city_name&facets%5B%5D=location&facets%5B%5D=business_category&facets%5B%5D=category&facets%5B%5D=schedule_type_id&facets%5B%5D=employee_class&facets%5B%5D=normalized_location&facets%5B%5D=job_function_id&facets%5B%5D=is_manager&facets%5B%5D=is_intern&offset=0&result_limit=${resLimit}&sort=relevant&latitude=&longitude=&loc_group_id=&loc_query=India&base_query=Software%20Development&city=&country=IND&region=&county=&query_options=&`;
 const name = 'amazon';
 
 const amazonHandler: Handler = {
@@ -10,7 +11,13 @@ const amazonHandler: Handler = {
     },
     getJobs: async () => {
         const amazonAPIRes = await axios.get(url);
-        const jobs: any[] = amazonAPIRes.data.jobs;
+        const rawJobs: any[] = amazonAPIRes.data.jobs;
+
+        // filters out jobs > a month 
+        const jobs: any[] = rawJobs.filter( job => {
+            return (Date.now()- Date.parse(job.posted_date)) / 1000 <= 2592000;
+        } )
+
         const result: Job[] = jobs.map(job => {
             return {
                 id: job.id,
